dce37821925030f58b29fc855e964842a49e090f
Ian <66477673+Weldify@users.noreply.github.com>
2023-07-18 15:20:54 +0300

    make retrieval O(1)

--- UniqueComponent.cs
+++ UniqueComponent.cs
@@ -1,24 +1,43 @@
 using System.Linq;
+using System.Collections.Generic;
 using Sandbox;
 
 namespace Ucom;
 
 public abstract partial class UniqueComponent : EntityComponent
 {
-	static uint NextUniqueId = 0;
+	static uint NextUniqueId = 1;
+	static Dictionary<uint, UniqueComponent> Components = new();
 
-	public static T FromId<T>(uint id) where T : UniqueComponent => GetAllOfType<T>().FirstOrDefault(u => u.UniqueId == id);
+	public static T FromId<T>(uint id) where T : UniqueComponent
+		=> Components.ContainsKey(id) ? Components[id] as T : null;
 
 	[Net] public uint UniqueId { get; private set; }
 
-	public UniqueComponent()
+	/// <summary>
+	/// Make sure to call this in your child class
+	/// </summary>
+	protected override void OnActivate()
 	{
-		if (Game.IsServer)
+		// The component is being reactivated or replicated to the client
+		if (UniqueId != default)
 		{
-			UniqueId = NextUniqueId;
-			NextUniqueId += 1;
+			AddToDict();
+			return;
 		}
+
+		if (!Game.IsServer) return;
+		
+		UniqueId = NextUniqueId;
+		NextUniqueId += 1;
+		AddToDict();
 	}
+
+	/// <summary>
+	/// Make sure to call this in your child class
+	/// </summary>
+	protected override void OnDeactivate() => Components.Remove(UniqueId);
+	void AddToDict() => Components.Add(UniqueId, this);
 }
 
 public abstract class UniqueComponent<T> : UniqueComponent where T : Entity
